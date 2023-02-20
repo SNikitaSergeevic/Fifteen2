3eb152ed05f868be424de0b70b08077b9ac96713
Nikita S <snikitasergeevich@gmail.com>
2023-02-20 19:37:42 +0300

    Initial Commit

---
+++ Fifteen2/Fifteen2App.swift
@@ -0,0 +1,17 @@
+//
+//  Fifteen2App.swift
+//  Fifteen2
+//
+//  Created by NikitaSergeevich on 20/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fifteen2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
